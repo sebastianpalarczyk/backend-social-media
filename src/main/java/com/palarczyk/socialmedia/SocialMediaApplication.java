36a885a0fc8251750fe446bcc58e0e593dcb443e
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-06 21:13:21 +0100

    configured database and model mapper

--- src/main/java/com/palarczyk/socialmedia/SocialMediaApplication.java
+++ src/main/java/com/palarczyk/socialmedia/SocialMediaApplication.java
@@ -2,12 +2,23 @@ package com.palarczyk.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialMediaApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+				.setMatchingStrategy(MatchingStrategies.LOOSE);
+		return modelMapper;
 	}
 
 }
