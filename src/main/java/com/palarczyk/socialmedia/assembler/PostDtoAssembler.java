c2fa66f54e41a8354a3e7ee76d115de4bb69c823
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-06 20:34:26 +0100

    added PostDtoAssembler

---
+++ src/main/java/com/palarczyk/socialmedia/assembler/PostDtoAssembler.java
@@ -0,0 +1,24 @@
+package com.palarczyk.socialmedia.assembler;
+
+import com.palarczyk.socialmedia.DTO.PostDto;
+import com.palarczyk.socialmedia.domain.Post;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostDtoAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public PostDtoAssembler(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public PostDto toDto (Post post){
+        return modelMapper.map(post, PostDto.class);
+    }
+
+    public Post fromDto(PostDto postDto){
+        return modelMapper.map(postDto, Post.class);
+    }
+}
