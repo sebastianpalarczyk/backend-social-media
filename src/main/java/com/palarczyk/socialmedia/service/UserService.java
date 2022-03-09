140a817dc4b2fdf56becf12a84e708f0abb94d03
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-09 13:20:22 +0100

    added UserService of user

---
+++ src/main/java/com/palarczyk/socialmedia/service/UserService.java
@@ -0,0 +1,30 @@
+package com.palarczyk.socialmedia.service;
+
+import com.palarczyk.socialmedia.domain.User;
+import com.palarczyk.socialmedia.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> all() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+}
