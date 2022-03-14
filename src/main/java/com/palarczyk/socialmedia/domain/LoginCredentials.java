382797955213617f544ed97ec4c5537c980ede06
sebastianpalarczyk <s.palarczyk12@gmail.com>
2022-03-14 19:17:21 +0100

    added constructors in LoginCredentials

--- src/main/java/com/palarczyk/socialmedia/domain/LoginCredentials.java
+++ src/main/java/com/palarczyk/socialmedia/domain/LoginCredentials.java
@@ -5,6 +5,14 @@ public class LoginCredentials {
     private String username;
     private String password;
 
+    public LoginCredentials() {
+    }
+
+    public LoginCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
