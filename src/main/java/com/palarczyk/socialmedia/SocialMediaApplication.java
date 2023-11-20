ea847f5ffa61d14997e89b0e81176bacdb6b1fe0
sebastianpalarczyk <s.palarczyk12@gmail.com>
2023-11-20 21:45:22 +0100

    added downloading of posts

--- src/main/java/com/palarczyk/socialmedia/SocialMediaApplication.java
+++ src/main/java/com/palarczyk/socialmedia/SocialMediaApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.palarczyk.socialmedia")
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/palarczyk/socialmedia/controller/FileController.java
+++ src/main/java/com/palarczyk/socialmedia/controller/FileController.java
@@ -1,9 +1,9 @@
 package com.palarczyk.socialmedia.controller;
 
 import com.palarczyk.socialmedia.service.FileService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FileController {
 
     private final FileService fileService;
